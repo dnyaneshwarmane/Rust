7fb693421deef0d828103b0e86f2f14228258884
VSPL-DV-LAP-014\Dnyaneshwar <dnyaneshwar@verificinet.com>
2022-02-23 00:57:59 +0530

    added build dir with lib

--- FFI/libhello/target/debug/deps/libhello.d
+++ FFI/libhello/target/debug/deps/libhello.d
@@ -1,5 +1,5 @@
-C:\Users\Dnyaneshwar\Desktop\RustL\220222\FFI\libhello\target\debug\deps\libhello.dll: src\lib.rs
+D:\REPOS\RUST1\RUST_Repo2\Rust\FFI\libhello\target\debug\deps\libhello.dll: src\lib.rs
 
-C:\Users\Dnyaneshwar\Desktop\RustL\220222\FFI\libhello\target\debug\deps\libhello.d: src\lib.rs
+D:\REPOS\RUST1\RUST_Repo2\Rust\FFI\libhello\target\debug\deps\libhello.d: src\lib.rs
 
 src\lib.rs:
---
+++ FFI/libhello/target/debug/libhello.d
@@ -0,0 +1,1 @@
+D:\REPOS\RUST1\RUST_Repo2\Rust\FFI\libhello\target\debug\libhello.dll: D:\REPOS\RUST1\RUST_Repo2\Rust\FFI\libhello\src\lib.rs
